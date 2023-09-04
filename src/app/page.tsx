37a1372d2dbb06b611facda1df34d2396e912503
Yulu Duan <92232261+YuluDuan@users.noreply.github.com>
2023-09-03 17:23:02 -0700

    fix the login out error (#29)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import LandingPageHeader from "@/components/LandingPage/LandingPageHeader/LandingPageHeader";
 import { db } from "@/lib/db";
 import { auth, clerkClient } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 async function getUsers() {
   return db.user.findMany().catch(() => {
@@ -9,12 +10,11 @@ async function getUsers() {
 }
 
 export default async function Home() {
-  const users = await getUsers();
-
   const { userId } = auth();
 
   if (userId) {
     const user = await clerkClient.users.getUser(userId);
+    redirect("/generate-blurb");
   }
   return (
     <>
